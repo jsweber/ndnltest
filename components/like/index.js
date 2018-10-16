6509948d7a16b29b8fad9cae129a8fad2a48f06b
ecuster <951092973@qq.com>
2018-10-17 01:56:14 +0800

    like

--- components/like/index.js
+++ components/like/index.js
@@ -4,20 +4,33 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    like: {
+      type: Boolean
+    },
+    count: {
+      type: Number
+    }
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-
+    defaultImg: '../images/like@2x.png',
+    activeImg: '../images/like-red@2x.png'
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    likeHandler(e){
+      let like = !this.properties.like
+      let count = like ? this.properties.count + 1 : this.properties.count -1
+      this.setData({
+        like,
+        count
+      })
+    }
   }
 })
